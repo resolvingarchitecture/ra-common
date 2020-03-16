14a5a71f7572e65aa7d2498d64c53c5e6a086460
objectorange <objectorange@1m5.io>
2020-03-15 19:34:21 -0700

    make fn public

--- src/lib.rs
+++ src/lib.rs
@@ -244,22 +244,22 @@ impl Slip {
 pub mod util {
     pub mod wait {
         use std::{thread, time};
-        fn wait_a_day(days: u64) {
+        pub fn wait_a_day(days: u64) {
             thread::sleep(time::Duration::from_secs(days * 24 * 60 * 60));
         }
-        fn wait_a_hour(hours: u64) {
+        pub fn wait_a_hour(hours: u64) {
             thread::sleep(time::Duration::from_secs(hours * 60 * 60));
         }
-        fn wait_a_minute(minutes: u64) {
+        pub fn wait_a_minute(minutes: u64) {
             thread::sleep(time::Duration::from_secs(minutes * 60));
         }
-        fn wait_a_sec(seconds: u64) {
+        pub fn wait_a_sec(seconds: u64) {
             thread::sleep(time::Duration::from_secs(seconds));
         }
-        fn wait_a_ms(millis: u64) {
+        pub fn wait_a_ms(millis: u64) {
             thread::sleep(time::Duration::from_millis(millis));
         }
-        fn wait_a_mic(mics: u64) {
+        pub fn wait_a_mic(mics: u64) {
             thread::sleep(time::Duration::from_micros(mics));
         }
     }
